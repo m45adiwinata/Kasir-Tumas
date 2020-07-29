e924d98633ddbf3d9a66e4557ceab5e5a7d7cfcb
m45adiwinata <m45adiwinata@gmail.com>
2020-07-29 16:45:51 +0800

    skip printer - perbaikan rekap

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -13,15 +13,6 @@ class PenjualanController extends Controller
 {
     public function store(Request $request)
     {
-        $mid = '123123456';
-        $store_name = 'TOKO SURADNYA GROSIR';
-        $store_address = 'Jl. Seririt - Gilimanuk, Banjar Asem Dajan Rurung';
-        $store_phone = 'Telp: 081 338 606 207, WA. 081 916 147 145';
-        $store_email = '';
-        $store_website = '';
-        $tax_percentage = 0;
-        $transaction_id = 'TX123ABC456';
-
         $data = new Penjualan;
         $data->total = $request->totalbelanja;
         $data->save();
@@ -46,27 +37,36 @@ class PenjualanController extends Controller
             ]);
             // $stok->jml_stok -= intval($request->jumlah[$key]);
         }
-        $printer = new ReceiptPrinter;
-        $printer->init(
-            config('receiptprinter.connector_type'),
-            config('receiptprinter.connector_descriptor')
-        );
-        $printer->setStore($mid, $store_name, $store_address, $store_phone, $store_email, $store_website);
-        // Add items
-        foreach ($items as $item) {
-            $printer->addItem(
-                $item['name'],
-                $item['qty'],
-                $item['price']
-            );
-        }
-        // Set tax
-        $printer->setTax($tax_percentage);
-        // Calculate total
-        $printer->calculateSubTotal();
-        $printer->calculateGrandTotal();
 
-        $printer->printReceipt();
+        // $mid = '123123456';
+        // $store_name = 'TOKO SURADNYA GROSIR';
+        // $store_address = 'Jl. Seririt - Gilimanuk, Banjar Asem Dajan Rurung';
+        // $store_phone = 'Telp: 081 338 606 207, WA. 081 916 147 145';
+        // $store_email = '';
+        // $store_website = '';
+        // $tax_percentage = 0;
+        // $transaction_id = 'TX123ABC456';
+        // $printer = new ReceiptPrinter;
+        // $printer->init(
+        //     config('receiptprinter.connector_type'),
+        //     config('receiptprinter.connector_descriptor')
+        // );
+        // $printer->setStore($mid, $store_name, $store_address, $store_phone, $store_email, $store_website);
+        // // Add items
+        // foreach ($items as $item) {
+        //     $printer->addItem(
+        //         $item['name'],
+        //         $item['qty'],
+        //         $item['price']
+        //     );
+        // }
+        // // Set tax
+        // $printer->setTax($tax_percentage);
+        // // Calculate total
+        // $printer->calculateSubTotal();
+        // $printer->calculateGrandTotal();
+
+        // $printer->printReceipt();
         // dd($printer);
         
         return redirect('/');
